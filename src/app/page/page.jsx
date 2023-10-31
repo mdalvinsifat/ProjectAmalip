3a475c768c5a98bb03d7cbaddb54375e38fd668d
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2023-10-31 09:58:46 -0700

    Update page.jsx

--- src/app/page/page.jsx
+++ src/app/page/page.jsx
@@ -10,7 +10,7 @@ const Page = () => {
   
 const Product = async()=>{
  try {
-  const response = await fetch("api/user")
+  const response = await fetch("/api/user")
   const res = await response.json()
   setData(res.product)
  } catch (error) {
